9560922c49c3f63ee1417e4103b8105d4c966ad3
alekperovvvv <zral360@mail.ru>
2024-11-18 10:44:58 +0300

    update5
    auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="adres col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" class="adres col-md-4 col-form-label text-md-end">{{ __('Ваш E-mail') }}</label>
 
                             <div class="names">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -28,7 +28,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Пароль') }}</label>
 
                             <div class="names">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -47,7 +47,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Запомнить меня') }}
                                     </label>
                                 </div>
                             </div>
@@ -56,14 +56,8 @@
                         <div class="passvor">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Войти') }}
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
                             </div>
                         </div>
                     </form>
