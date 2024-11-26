a812c1f45b156eefac7c9503d2583f5c0d5a753c
ksenia0511122 <lkettimail2005@gmail.com>
2024-11-26 12:57:39 +0300

    upd20
    qwerty

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ReservationController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\DishController;
 
+
+
+// Главная страница
 Route::get('/', function () {
-    return view('one');
+    return view('one'); // Это ваша главная страница
 });
-
+// Страница меню
+Route::get('/menu', function () {
+    return view('menu'); // Это ваша страница меню
+});
 Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::post('/reserve', [ReservationController::class, 'store'])->name('reserve.store');
+Route::get('/', [DishController::class, 'index'])->name('dishes.index');
