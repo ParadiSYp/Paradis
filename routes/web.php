6d39a5b202befbe3bfac9b7ef18378e9f39a34f7
Ахмедова Мадина <md27ahmedova@gmail.com>
2024-12-11 21:27:55 +0300

    upd26
    efw

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,19 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\DishController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
 
 Auth::routes();
 
 // Главная страница
 Route::get('/', function () {
-    return view('one'); // Это ваша главная страница
+    return view('basket'); // Это ваша главная страница
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::post('/reserve', [ReservationController::class, 'store'])->name('reserve.store');
 Route::get('/menu', [DishController::class, 'index'])->name('dishes.index');
 
+Route::middleware('auth')->group(function () {
+Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
+});
